77f672d5f4d7b7b9b20bd07058dd58744f55836c
adzhazhev <alex.dzhazhev@gmail.com>
2014-10-09 14:42:16 +0300

    added escape-html module

--- public/app/account/profileCtrl.js
+++ public/app/account/profileCtrl.js
@@ -2,7 +2,7 @@ app.controller('ProfileCtrl', function($scope, $location, auth, identity) {
     $scope.user = {
         firstName: identity.currentUser.firstName,
         lastName: identity.currentUser.lastName
-    }
+    };
 
     $scope.update = function(user) {
         auth.update(user).then(function() {
